2f53d3df526c1d6fe6ce0c440f17f5ed0949535d
Filip Stenbacka <filip.stenbacka@trimble.com>
2023-03-18 01:24:13 +0200

    Lint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
         "@typescript-eslint/no-inferrable-types": "off",
         "@typescript-eslint/no-unsafe-return": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-non-null-assertion": "off"
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off"
     }
 };
